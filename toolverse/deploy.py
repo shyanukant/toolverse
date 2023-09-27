099bdabd47526dd026023486afcf7c3cf2903714
shyanukant rathi <rathishyanukant@gmail.com>
2023-09-27 19:09:00 +0530

    add connection string

--- toolverse/deploy.py
+++ toolverse/deploy.py
@@ -1,6 +1,6 @@
 import os
 from .settings import *
-from .settings import BASE_DIR, MIDDLEWARE, DEBUG
+from .settings import  MIDDLEWARE, DEBUG
 
 if DEBUG:
     DEBUG = False
@@ -8,14 +8,16 @@ if DEBUG:
 CSRF_TRUSTED_ORIGINS = ['https://'+ os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
 
 
-hostname = os.environ['DBHOST']
+connection_string = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
+parameter = {pair.split('=')[0] : pair.split('=')[1] for pair in connection_string.split(' ')}
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ['DBNAME'],
-        'HOST': hostname + ".postgres.database.azure.com",
-        'USER': os.environ['DBUSER'],
-        'PASSWORD': os.environ['DBPASS'] 
+        'NAME': parameter['dbname'],
+        'HOST': parameter['host'],
+        'USER': parameter['user'],
+        'PASSWORD': parameter['password'],
+        'PORT' : parameter['port'] 
     }
 }
 # Email send the user (email server)
