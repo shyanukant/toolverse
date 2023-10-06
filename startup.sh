a1b5e93d7dbaf482ea4b4c3cfce4fa2bfa06554c
shyanukant rathi <rathishyanukant@gmail.com>
2023-10-06 22:23:38 +0530

    update startup file

--- startup.sh
+++ startup.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
-
-# Define the path to your virtual environment
-venv_path=/home/site/wwwroot/antenv
-
-# Activate the virtual environment
-source $venv_path/bin/activate
-
-# Change directory to your Django app's root folder
-cd /home/site/wwwroot
+export PYTHONPATH=$PYTHONPATH:"/home/site/wwwroot/.venv/lib/site-packages"
 
 # Install dependencies from requirements.txt (optional, but good practice)
 pip install -r requirements.txt
 
 # Run your Django app using Gunicorn
-gunicorn toolverse.wsgi:application --bind 0.0.0.0:$PORT
+GUNICORN_CMD_ARGS="--timeout 600 --access-logfile '-' --error-logfile '-' --chdir=/home/site/wwwroot" gunicorn  toolverse.wsgi:application --bind 0.0.0.0:$PORT
