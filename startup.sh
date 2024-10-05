1eb4290427dfc7a64551086c22a62ed50dd08661
Shyanukant Rathi <72182251+shyanukant@users.noreply.github.com>
2024-10-05 08:12:14 +0000

    render deploy

--- startup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-export PYTHONPATH=$PYTHONPATH:"/home/site/wwwroot/.venv/lib/site-packages"
-
-# Install dependencies from requirements.txt (optional, but good practice)
-pip install -r requirements.txt
-
-# Run your Django app using Gunicorn
-GUNICORN_CMD_ARGS="--timeout 600 --access-logfile '-' --error-logfile '-' --chdir=/home/site/wwwroot" gunicorn  toolverse.wsgi:application --bind 0.0.0.0:$PORT
